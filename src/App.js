48fa0b7a53057bf0aa39c966b04a688cd44af150
Cryptic Stargazer <78745089+coinbrute@users.noreply.github.com>
2023-01-05 22:08:37 +0000

    have base application working. need to add actual conversion and package as extension.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Editor from './components/editor';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+        <Editor />
+
+    );
+  }
+}
+
+export default App;
