3918f6071af4b6c62d35f4ba2d6ff3426096e0a3
Cryptic Stargazer <78745089+coinbrute@users.noreply.github.com>
2023-01-10 05:21:51 +0000

    stubbed second component. will use this for footer. need to make navbar and side components for adsense as well as stub the rest of the converter imports. mvp will be three converters back and forth so 6 total options.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Editor from './components/editor';
+import Footer from './components/footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
+      <div>
         <Editor />
-
+        <br/>
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <div>FOOTER WORKS!</div>
+        )
+    }
+}
+export default Footer;
